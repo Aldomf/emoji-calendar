17a01b7e1136090407256383658e9155fb9d6974
Aldo Miralles <alditomiralles3@gmail.com>
2025-09-26 15:45:51 -0400

    adding base and build to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/emoji-calendar/",  // <- agrega esto
+  build: {
+    outDir: "dist"  // o "build" si cambiaste el outDir
+  }
+});
