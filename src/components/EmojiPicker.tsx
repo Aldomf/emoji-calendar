6eee14e212ca408ab87823493499c4e733ebb895
Aldo Miralles <alditomiralles3@gmail.com>
2025-09-27 18:56:14 -0400

    adding alert before deleting emoji

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -7,16 +7,38 @@ type Props = {
   close: () => void;
 };
 
-export default function EmojiPicker({ selectedKey, setEmoji, clearEmoji, close }: Props) {
+export default function EmojiPicker({
+  selectedKey,
+  setEmoji,
+  clearEmoji,
+  close,
+}: Props) {
   return (
     <div className="picker">
       <div className="picker-title">Choose emoji for {selectedKey}</div>
       <div className="picker-row">
         {EMOJIS.map((e) => (
-          <button key={e} className="emoji-btn" onClick={() => setEmoji(selectedKey, e)}>{e}</button>
+          <button
+            key={e}
+            className="emoji-btn"
+            onClick={() => setEmoji(selectedKey, e)}
+          >
+            {e}
+          </button>
         ))}
-        <button className="clear-btn" onClick={() => clearEmoji(selectedKey)}>Clear</button>
-        <button className="close-btn" onClick={close}>Cancel</button>
+        <button
+          className="clear-btn"
+          onClick={() => {
+            if (window.confirm("Are you sure you want to clear this emoji?")) {
+              clearEmoji(selectedKey);
+            }
+          }}
+        >
+          Clear
+        </button>
+        <button className="close-btn" onClick={close}>
+          Cancel
+        </button>
       </div>
     </div>
   );
